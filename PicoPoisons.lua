a9ac0a5fc3da9bbac05fd3a1d9094b9b3e2a6a56
Janis Born <janis.born@rwth-aachen.de>
2015-10-17 14:38:40 +0200

    initial commit

---
+++ PicoPoisons.lua
@@ -0,0 +1,44 @@
+function PicoPoisons_Init()
+    -- modify enchant icons to add a counter label
+    for i = 1, 2 do
+        local enchantButton = getglobal("TempEnchant" .. i)
+        local label = enchantButton:CreateFontString(enchantButton:GetName() .. "ChargesLabel", "ARTWORK", "NumberFontNormal")
+        label:SetPoint("BOTTOMLEFT", enchantButton, "BOTTOMLEFT", 1, 1)
+        label:SetText("lol")
+        --label:Hide()
+    end
+    
+    -- hook BuffFrame_Enchant functions
+    PicoPoisons_Old_BuffFrame_Enchant_OnUpdate = BuffFrame_Enchant_OnUpdate
+    BuffFrame_Enchant_OnUpdate = PicoPoisons_BuffFrame_Enchant_OnUpdate
+end
+
+function PicoPoisons_BuffFrame_Enchant_OnUpdate(elapsed)
+    -- original functionality
+    PicoPoisons_Old_BuffFrame_Enchant_OnUpdate(elapsed)
+    
+    local hasMainHandEnchant, mainHandExpiration, mainHandCharges, hasOffHandEnchant, offHandExpiration, offHandCharges = GetWeaponEnchantInfo()
+    local buttonIndex = 1
+    if hasOffHandEnchant then
+        local chargesLabel = getglobal("TempEnchant" .. buttonIndex .. "ChargesLabel")
+        chargesLabel:SetText(offHandCharges)
+        if offHandCharges > 0 then
+            chargesLabel:Show()
+        else
+            chargesLabel:Hide()
+        end
+        buttonIndex = buttonIndex + 1
+    end
+    if hasMainHandEnchant then
+        local chargesLabel = getglobal("TempEnchant" .. buttonIndex .. "ChargesLabel")
+        chargesLabel:SetText(mainHandCharges)
+        if mainHandCharges > 0 then
+            chargesLabel:Show()
+        else
+            chargesLabel:Hide()
+        end
+        buttonIndex = buttonIndex + 1
+    end
+end
+
+PicoPoisons_Init()
